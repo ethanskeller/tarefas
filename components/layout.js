ca9612c58152c999b73e063220e8d21f05fea1e8
Henrique Wildes <henriquewildes@gmail.com>
2020-07-28 03:09:13 -0400

    Apply style in sidebar

--- components/layout.js
+++ components/layout.js
@@ -5,10 +5,10 @@ import Sidebar from './sidebar';
 const Layout = ({ children }) => {
     return (
         <div className="flex flex-wrap">
-            <aside className="lg:h-auto bg-white hidden lg:block lg:static fixed h-full w-full lg:w-1/4 xl:w-1/6 p-5">
+            <aside className="lg:h-auto bg-white hidden lg:block lg:static fixed h-full w-full lg:w-1/4 xl:w-1/6 shadow-lg">
                 <Sidebar />
             </aside>
-            <div className="min-h-screen w-full lg:static lg:w-3/4 xl:w-5/6 bg-gray-200">
+            <div className="min-h-screen w-full lg:static lg:w-3/4 xl:w-5/6 bg-gray-200 shadow-inner">
                 <PageHeader />
                 <main className="p-10">
                     {children}
--- components/sidebar.js
+++ components/sidebar.js
@@ -4,29 +4,39 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 const Sidebar = () => {
     return (
         <>
-            <h1>Workly</h1>
-            <a className="text-center my-5 block">
-                <FontAwesomeIcon className="mr-3" icon={'plus'} />
-                New Task
-            </a>
+            <div className="p-8">
+                <h1>Workly</h1>
+                <a className="text-center my-5 block bg-purple-200 text-purple-700 p-3 font-bold rounded-full rounded-tl-none">
+                    <FontAwesomeIcon className="mr-3" icon={'plus'} />
+                    New Task
+                </a>
+            </div>
             <nav className="block">
                 <ul className="py-1">
-                    <li className="my-5 flex items-center">
-                        <FontAwesomeIcon className="mr-3" size="lg" icon={'home'} fixedWidth />
-                        Home
+                    <li className="flex items-center py-3 px-8 border-l-8 border-white">
+                        <a>
+                        <   FontAwesomeIcon className="mr-3" size="lg" icon={'home'} fixedWidth />
+                            Home
+                        </a>
                     </li>
-                    <li className="my-5 flex items-center">
-                        <FontAwesomeIcon className="mr-3" size="lg" icon={'tasks'} fixedWidth />
-                        My Tasks
+                    <li className="flex items-center py-3 px-8 border-l-8 border-purple-500">
+                        <a>
+                        <   FontAwesomeIcon className="mr-3" size="lg" icon={'tasks'} fixedWidth />
+                            My Tasks
+                        </a>
                     </li>
-                    <li className="my-5 flex items-center">
-                        <FontAwesomeIcon className="mr-3" size="lg" icon={'clipboard'} fixedWidth />
-                        Task Catalog
+                    <li className="flex items-center py-3 px-8 border-l-8 border-white">
+                        <a>
+                        <   FontAwesomeIcon className="mr-3" size="lg" icon={'clipboard'} fixedWidth />
+                            Task Catalog
+                        </a>
                     </li>
-                    <li className="my-5 flex items-center">
-                        <FontAwesomeIcon className="mr-3" size="lg" icon={'users'} fixedWidth />
-                        Community
-                        <FontAwesomeIcon className="ml-3" size="xs" icon={'chevron-down'} fixedWidth />
+                    <li className="flex items-center py-3 px-8 border-l-8 border-white">
+                        <a>
+                            <FontAwesomeIcon className="mr-3" size="lg" icon={'users'} fixedWidth />
+                            Community
+                            <FontAwesomeIcon className="ml-3" size="xs" icon={'chevron-down'} fixedWidth />
+                        </a>
                     </li>
                 </ul>
             </nav>
